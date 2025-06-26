7945fd4d76abe3aaa885a9617f290060f8451c7c
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-26 10:20:22 +0700

    Sisipkan node baru sebagai anak kiri jika nilainya lebih kecil dari parent

--- Binarysearchtree.cpp
+++ Binarysearchtree.cpp
@@ -59,7 +59,15 @@ class BinaryTree
             //5b:exit
             return;
         }
-       
+       //step 6 : If the value in the data field of nee node is less than that  of parent
+       if(x < parent->info)
+       {
+          //6a: make the left child of parent point to the new node
+          parent->leftchild = newNode;
+
+          //6b:exit
+          return;
+       }
 
     }
 
